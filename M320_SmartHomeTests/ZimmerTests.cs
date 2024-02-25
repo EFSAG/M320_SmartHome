d923471ce49225a9d786f0640b415cdd6845bed9
EFSAG <e.bora.saglam@gmail.com>
2024-02-25 10:15:07 +0100

    Test erstellt, Efe Bora Saglam.

--- M320_SmartHomeTests/ZimmerTests.cs
+++ M320_SmartHomeTests/ZimmerTests.cs
@@ -14,56 +14,36 @@ namespace M320_SmartHome.Tests
         [TestMethod()]
         public void TestHeizungsventilBei20GradNichtÖffnet()
         {
-            // Arrange
             var zimmer = new Heizungsventil(new Kueche());
             var wetterdaten = new Wetterdaten(20, 10, false);
-
-            // Act
             zimmer.VerarbeiteWetterdaten(wetterdaten);
-
-            // Assert
             Assert.IsFalse(zimmer.HeizungsventilOffen);
         }
 
         [TestMethod]
         public void TestHeizungsventilBei19GradNichtÖffnet()
         {
-            // Arrange
             var zimmer = new Heizungsventil(new BadWC());
             var wetterdaten = new Wetterdaten(19, 10, false);
-
-            // Act
             zimmer.VerarbeiteWetterdaten(wetterdaten);
-
-            // Assert
             Assert.IsFalse(zimmer.HeizungsventilOffen);
         }
 
         [TestMethod]
         public void TestHeizungsventilBeiMinus25GradÖffnet()
         {
-            // Arrange
             var zimmer = new Heizungsventil(new Schlafzimmer());
             var wetterdaten = new Wetterdaten(-25, 10, false);
-
-            // Act
             zimmer.VerarbeiteWetterdaten(wetterdaten);
-
-            // Assert
             Assert.IsTrue(zimmer.HeizungsventilOffen);
         }
 
         [TestMethod()]
         public void TestHeizungsventilBei35GradNichtÖffnet()
         {
-            // Arrange
             var zimmer = new Heizungsventil(new Wohnzimmer());
             var wetterdaten = new Wetterdaten(35, 10, false);
-
-            // Act
             zimmer.VerarbeiteWetterdaten(wetterdaten);
-
-            // Assert
             Assert.IsFalse(zimmer.HeizungsventilOffen);
         }
     }
